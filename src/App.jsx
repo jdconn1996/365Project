9ae6e9070af965d0f356afc5a8f76850643500ab
unknown <Zachariah586044@live.missouristate.edu>
2025-11-19 14:19:29 -0500

    Trying to get a carousel going, despite all my work still only showing one card per row..

--- src/App.jsx
+++ src/App.jsx
@@ -7,73 +7,62 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <div
-          style={{maxWidth: "1080px"}}
-        >
-          <h1>Pokédex Mobile</h1>
-            <div>
-              <input
-                placeholder="Type Pokémon name"
-              />
-            </div>
-            <div>
-              <button>
-                ❤Favorites
-              </button>
-              <button>
-                History
-              </button>
-            </div>
-            <h3>Recently viewed</h3>
-          <div class="container">
-            <div class="row">
-            <a href="https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)" class="btn btn-primary">
+    
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     <div className="cards-wrapper">
+      <a href="https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)" class="btn btn-primary">
             
-            <div className="card" style={{ width: "18rem" }}>
-           <img class="card-img-top" src="src/Pokémon_Pikachu_art.png" alt="Card image cap"></img>
+            <div className="card" >
+           <img  src="src/Pokémon_Pikachu_art.png" alt="Card image cap"></img>
            <div class="card-body">
            <h5 class="card-title">Pikachu</h5>
            </div>
             </div>
             </a>
-            <a href="https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)" class="btn btn-primary">
+     </div>
+    </div>
+    <div class="carousel-item">
+      <div className="cards-wrapper">
+        <a href="https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)" class="btn btn-primary">
             
-            <div className="card" style={{ width: "18rem" }}>
-           <img class="card-img-top" src="src/Pokémon_Pikachu_art.png" alt="Card image cap"></img>
+            <div className="card" >
+           <img  src="src/Pokémon_Pikachu_art.png" alt="Card image cap"></img>
            <div class="card-body">
            <h5 class="card-title">Pikachu</h5>
            </div>
             </div>
             </a>
-            </div>
-            </div>
-            <div>
-              {["Pokemon 1", "Pokemon 2", "Pokemon 3", "Pokemon 4"].map((name, pokemon) => (
-              <div key={pokemon}>
-                <div
-                    style={{ width: 50, height: 20 }}
-                ></div>
-                <text>{name}</text>
-              </div>
-              ))}
-            </div>
-            <h4>Suggested Pokémon</h4>
-            <div>
-              {["Pokemon 1", "Pokemon 2", "Pokemon 3", "Pokemon 4"].map((name, pokemon) => (
-              <div key={pokemon}>
-                <div
-                  style={{ height: 20 }}
-                ></div>
-                <text>{name}</text>
-              </div>
-              ))}
-            </div>
-        </div>
       </div>
-    </>
-  )
+    </div>
+    <div class="carousel-item">
+      <div className="cards-wrapper">
+        <a href="https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)" class="btn btn-primary">
+            
+            <div className="card" >
+           <img src="src/Pokémon_Pikachu_art.png" alt="Card image cap"></img>
+           <div class="card-body">
+           <h5 class="card-title">Pikachu</h5>
+           </div>
+            </div>
+            </a>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>
+
+      )
 }
 
 export default App
