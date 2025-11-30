4634c43ca7f77dc8b64e7f18768de7fd2c157638
unknown <Zachariah586044@live.missouristate.edu>
2025-11-29 20:17:33 -0500

    Tried to get router working for other pages, but only getting blank pages for everything but home page. Just going to leave it for tomorrow because I am too frustrated

--- src/App.jsx
+++ src/App.jsx
@@ -6,71 +6,25 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import MainCC from './maincc.jsx';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate, Navigate } from 'react-router-dom';
 
-const HomePage = () => {
-  
-  const recentItems = [
-    { name: 'Pikachu', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
-    { name: 'Charmander', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
-    { name: 'Squirtle', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
-      { name: 'Pikachu', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
-      { name: 'Charmander', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
-      { name: 'Squirtle', image: 'src/9millyBilly.jpeg', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' }
-  ]; 
+import HomePage from './HomePage.jsx';
 
-  const suggestedItems = [
-    { name: 'Bulbasaur', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
-    { name: 'Eevee', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Eevee_(Pok%C3%A9mon)' },
-    { name: 'Snorlax', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' }
-  ];
-
-  const bottomMenu = [
-    { name: 'Compare', image: 'src/compare.png', url: 'https://bulbapedia.bulbagarden.net/wiki/File:JohtoSinnoh_BF.png' },
-    { name: 'Battle Sim', image: 'src/battlesim.png', url: 'https://bulbapedia.bulbagarden.net/wiki/File:EmeraldBFLogo.png' },
-    { name: 'Change Game', image: 'src/changegame.png', url: 'https://bulbapedia.bulbagarden.net/wiki/File:Pok%C3%A9mon_VG_logo.png' }
-  ];
-  return (
-    
-    <>
-      <div style={{width: "100%"}}>
-        <div
-          style={{width: "100%"}}
-        >
-
-            <div>
-              <button className="btn btn-primary">
-                Create Account!
-              </button>
-              <input className={"form-control"} style={{width: "60%", margin: "1rem auto"}}
-                placeholder="Type Pokémon name"
-              />
-            </div>
-            <div style={{marginTop: "10px"}}>
-              <button className="btn btn-primary" style={{marginRight: "10px"}}>
-                ❤ Favorites
-              </button>
-              <button className="btn btn-primary">
-                History
-              </button>
-              
-            </div>
-            <MainCC title="Recently Viewed" items={recentItems}/>
-            <MainCC title="Suggested Pokémon" items={suggestedItems} />
-            <MainCC title="Menu" items={bottomMenu} />
-        </div>
-      </div>
-    </>
-    
-  )
-}
 
 
 function App() {
   
   return (
     <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          
+        </nav>
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />}>
         </Route>
+        
+        
       </Routes>
     </Router>
   );
---
+++ src/compare.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import MainCC from './maincc.jsx';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate, Navigate } from 'react-router-dom';
+
+const compare = () => {
+  
+  const recentItems = [
+    { name: 'Pikachumew', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
+    { name: 'Charmander', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
+    { name: 'Squirtle', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
+      { name: 'Pikachu', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
+      { name: 'Charmander', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
+      { name: 'Squirtle', image: 'src/9millyBilly.jpeg', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' }
+  ]; 
+
+  const suggestedItems = [
+    { name: 'Bulbasaur', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' },
+    { name: 'Eevee', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Eevee_(Pok%C3%A9mon)' },
+    { name: 'Snorlax', image: 'src/Pokémon_Pikachu_art.png', url: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)' }
+  ];
+
+  const bottomMenu = [
+    { name: 'Compare', image: 'src/compare.png', url: 'https://bulbapedia.bulbagarden.net/wiki/File:JohtoSinnoh_BF.png' },
+    { name: 'Battle Sim', image: 'src/battlesim.png', url: 'https://bulbapedia.bulbagarden.net/wiki/File:EmeraldBFLogo.png' },
+    { name: 'Change Game', image: 'src/changegame.png', url: 'https://bulbapedia.bulbagarden.net/wiki/File:Pok%C3%A9mon_VG_logo.png' }
+  ];
+  return (
+    
+    <>
+      <div style={{width: "100%"}}>
+        <div
+          style={{width: "100%"}}
+        >
+
+            <div>
+              <button className="btn btn-primary">
+                Create Account!
+              </button>
+              <input className={"form-control"} style={{width: "60%", margin: "1rem auto"}}
+                placeholder="Type Pokémon name"
+              />
+            </div>
+            <div style={{marginTop: "10px"}}>
+              <button className="btn btn-primary" style={{marginRight: "10px"}}>
+                ❤ Favorites
+              </button>
+              <button className="btn btn-primary">
+                History
+              </button>
+              
+            </div>
+            <MainCC title="Recently Viewed" items={recentItems}/>
+            <MainCC title="Suggested Pokémon" items={suggestedItems} />
+            <MainCC title="Menu" items={bottomMenu} />
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+export default compare
