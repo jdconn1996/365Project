984246f6304201125a566eafe619f9beeba5dc39
Joshua Conn <joshua.conn96@gmail.com>
2025-12-01 11:58:46 -0600

    Made the textDecoration on the card none

--- src/card.jsx
+++ src/card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Card({ name, image, url }) {  
     return (
-        <a href={url} className="p-0 d-block" style={{ display: "block" }}>
+        <a href={url} className="p-0 d-block" style={{ display: "block", textDecoration: "none" }}>
             <div className="card app-card" style={{ width: "100%", maxWidth: "320px", backgroundColor: "#B3A125",boxShadow: "2px 5px 7px", margin: "15px" }}>
                 <img className="card-img-top img-fluid" src={image} alt={name} style={{borderRadius: "12px",  width: "100%", height: "auto", objectFit: "cover"}} />
                 <div className="card-body text-center" style={{ textAlign: "center" }}>
